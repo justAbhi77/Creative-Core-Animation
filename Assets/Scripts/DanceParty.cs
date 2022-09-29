2bb35a7a4f0b6bf8a123e21ce2a95b89fb407a3d
justAbhi77 <ojhaabhinav2016@gmail.com>
2022-09-29 17:11:25 +0530

    Completed #4 tutorial

--- Assets/Scripts/DanceParty.cs
+++ Assets/Scripts/DanceParty.cs
@@ -6,15 +6,11 @@ public class DanceParty : MonoBehaviour
 {
     private Animator anim;
     
-
-    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
        
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
